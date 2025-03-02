b29530ea21d9ba3d97c10faac0881b151cb2b145
ezzat <ezzatw122@gmail.com>
2025-03-02 07:25:20 +0200

    Array

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include "Array.cpp"
+
+using namespace std;
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
+    Array<int> v(10);
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    Array<int> v22(1);
+    v.Display();
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
     // <b>lang</b> variable name to see how CLion can help you rename it.
-
+    v22 = v;
 
     return 0;
 }
